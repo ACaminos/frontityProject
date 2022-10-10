7d268e9c0eb1af00d64a36c2c2d21fabc2ea5e46
Ariel Caminos <caminosariel@gmail.com>
2022-10-10 15:57:01 -0300

    first steps with frontity

--- frontity.settings.js
+++ frontity.settings.js
@@ -8,44 +8,52 @@ const settings = {
     }
   },
   "packages": [
+    // Para poder dar de alta un nuevo tema, se elimina esta linea que es la que contiene el predeterminado.
+    // {
+    //   "name": "@frontity/mars-theme",
+    //   "state": {
+    //     "theme": {
+    //       "menu": [
+    //         [
+    //           "Home",
+    //           "/"
+    //         ],
+    //         [
+    //           "Nature",
+    //           "/category/nature/"
+    //         ],
+    //         [
+    //           "Travel",
+    //           "/category/travel/"
+    //         ],
+    //         [
+    //           "Japan",
+    //           "/tag/japan/"
+    //         ],
+    //         [
+    //           "About Us",
+    //           "/about-us/"
+    //         ]
+    //       ],
+    //       "featured": {
+    //         "showOnList": false,
+    //         "showOnPost": false
+    //       }
+    //     }
+    //   }
+    // },
+
+
+    // Se agrega esta linea que contiene el nuevo theme
     {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "Nature",
-              "/category/nature/"
-            ],
-            [
-              "Travel",
-              "/category/travel/"
-            ],
-            [
-              "Japan",
-              "/tag/japan/"
-            ],
-            [
-              "About Us",
-              "/about-us/"
-            ]
-          ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
-          }
-        }
-      }
+      name : "my-first-theme"
     },
     {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "https://test.frontity.org"
+          // "url": "http://tvpabe.tvpublica.com.ar/"
+          url: "https://test.frontity.org",
         }
       }
     },
---
+++ packages/my-first-theme/src/components/index.js
@@ -0,0 +1,34 @@
+import React from "react"
+import connect from "@frontity/connect"
+import Link from "@frontity/components/link"
+import Switch from "@frontity/components/switch"
+
+import List from "./list"
+import Post from "./post"
+
+const Root = ({state}) => {
+    const data = state.source.get(state.router.link)
+  return (
+    <>
+      <h1>Hello Frontity</h1>
+      <p>Current URL: {state.router.link}</p>
+      <nav>
+        <Link link="/">Home</Link>
+        <br/>
+        <Link link="/page/2">More posts</Link>
+        <br/>
+        <Link link="/about-us">About Us</Link>
+      </nav>
+      <hr/>
+      <main>
+        <Switch>
+            <List when={data.isArchive} />
+            <Post when={data.isPost} />
+            <Post when={data.isPage} />
+        </Switch>
+      </main>
+    </>
+  )
+}
+
+export default connect(Root)
